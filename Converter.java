8c82978d5dbca50722d6e7c8cbcbbf4be96e7101
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2022-11-12 13:30:29 -0800

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -1,6 +1,6 @@
 package final_group_project;
 
 public interface Converter {
-	public void converter(double money); //convert the currency to another base on the currency rate. This method will use in transit method
+	public void converter(double money, double rate); //convert the currency to another base on the currency rate. This method will use in transit method
 	public void transit(double money, int bank1, int bank2); //use switch state or if statement 
 }
